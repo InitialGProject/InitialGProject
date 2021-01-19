01b19180cc853e2b4f38b273129d2228a0f904c2
JuanSanz2000 <49302792+JuanSanz2000@users.noreply.github.com>
2021-01-19 18:31:06 +0100

    cambioNombreProyecto_integracion_jQuery_lateralbar

--- src/app/lateralbar/lateralbar.component.ts
+++ src/app/lateralbar/lateralbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-lateralbar',
   templateUrl: './lateralbar.component.html',
   styleUrls: ['./lateralbar.component.scss']
 })
-export class LateralbarComponent implements OnInit {
+export class LateralbarComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    //Toggle Click Function
+    $("#menu-toggle").click(function (e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
   }
 
 }
