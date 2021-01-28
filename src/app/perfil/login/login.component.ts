dcea201ad97758a11abe993c1b3558055fa7efb1
danedelea <danedelea@github .com>
2021-01-28 18:36:07 +0100

    creacion de servicios para cada modulo

--- src/app/perfil/login/login.component.ts
+++ src/app/perfil/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthGuardService } from '../../services/auth-guard.service';
+import { AuthGuardService } from '../services/auth-guard.service';
 
 @Component({
   selector: 'app-login',
