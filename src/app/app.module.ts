02deadd762ad0117a1d68a02703bfab51a16eb6a
danedelea <danedelea@github .com>
2021-01-28 19:22:18 +0100

    forChild-perfil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { AudiovisualesModule } from './audiovisuales/audiovisuales.module';
 import { JuegosModule } from './juegos/juegos.module';
 import { TorneosModule } from './torneos/torneos.module';
 import { ForoModule } from './foro/foro.module';
+import { PerfilModule } from './perfil/perfil.module';
 
 // modulos extra
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -36,6 +37,7 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
     JuegosModule,
     TorneosModule,
     ForoModule,
+    PerfilModule,
     BrowserAnimationsModule
   ],
   providers: [
