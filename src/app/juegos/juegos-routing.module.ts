b8a7988b246d8456e0e39566ddfe6876db7b4cc5
mithjack <alexlopez168@hotmail.com>
2021-02-05 13:00:57 +0100

    actualizado modulo juegos

--- src/app/juegos/juegos-routing.module.ts
+++ src/app/juegos/juegos-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { VistaJuegosComponent } from './vista-juegos/vista-juegos.component';
 import { JuegoComponent } from './juego/juego.component';
+import { DetalleJuegosComponent } from './detalle-juegos/detalle-juegos.component';
 import { LayoutComponent } from './layout/layout.component';
 
 const juegos: Routes = [
@@ -10,7 +11,8 @@ const juegos: Routes = [
         path: 'vista-juegos', component: LayoutComponent,
         children: [
             { path: 'vista-juegos', component: VistaJuegosComponent },
-            { path: 'juego', component: JuegoComponent }
+            { path: 'juego', component: JuegoComponent },
+            { path: 'detalle-juegos', component: DetalleJuegosComponent }
         ]
     }
 ];
