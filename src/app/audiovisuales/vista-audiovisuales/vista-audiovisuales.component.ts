455973a48ecf2541aa2a77fe265268602f10a083
Marta Pretel <mpretel3@iesfuentesanluis.org>
2021-02-08 20:28:58 +0100

    titulo videos y reproductor

--- src/app/audiovisuales/vista-audiovisuales/vista-audiovisuales.component.ts
+++ src/app/audiovisuales/vista-audiovisuales/vista-audiovisuales.component.ts
@@ -1,38 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatDialog } from '@angular/material/dialog';
+
+
 
 let apiKey = "AIzaSyAeoW2rf-X11QTEKKOCgRZ6VZX3vA7xfiU&"
 let url = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&maxResults=15&key="
 let word = "&q="
-
 @Component({
   selector: 'app-vista-audiovisuales',
   templateUrl: './vista-audiovisuales.component.html',
   styleUrls: ['./vista-audiovisuales.component.scss']
 })
-
 export class VistaAudiovisualesComponent implements OnInit {
 
-  constructor() { }
-
-  response = [
-    "DcmZpcZl0qg", "ktDItuU-G8w", "cSwuvXKqfRM", "sxDSAl08slU", "orDRnUXu-Rw",
-    "FLpiDFE991g", "9Fcx_UYTAIE", "tUicuMsgUGs", "HTssTMmCNfM", "FApX-M_DrDc",
-    "TLa52NO8pms", "-os4vj6vEo8", "G51GkSmQAmQ", "YG5rwL3sqzg", "09l3Px5A-LU"
-  ]
+  constructor(public dialog: MatDialog, private sanitizer:DomSanitizer) { }
 
   ngOnInit(): void {
-    this.searchFunction("gameplay")
+   //this.searchFunction("gameplay")
   }
-
+  videoUrl
+  mostrarVideos = true
+  videoSeleccionado = false
+  videdoTitles = ["WEREWOLF THE APOCALYPSE EARTHBLOOD ...", "THE LAST OF US 2 PS5 Gameplay 4K HD...", "WEREWOLF THE APOCALYPSE EARTHBLOOD ...", "STAR WARS BATTLEFRONT 2 ►SE VIENE l...", "🔴 NEW Airship Map In Among Us Game...", "HITMAN 3 Walkthrough Gameplay Part ...", "ASSASSIN&#39;S CREED VALHALLA Walkt...", "CALL OF DUTY BLACK OPS COLD WAR Cam...", "RESIDENT EVIL 8 PS5 Walkthrough Gam...", "THE MEDIUM Walkthrough Gameplay Par...", "Ajjubhai 45 Glo Wall World Record D...", "BATMAN GOTHAM KNIGHTS Gameplay 4K (...", "Little Nightmares - Gameplay Walkth...", "theRadBrad", "The Medium - O INÍCIO do gameplay d..."]
+  response =  ["TA4IKZVYTGI", "orDRnUXu-Rw", "LCq8nl7Jcz8", "v2EQJsnFch8", "nlLLNN0kx8Y", "DcmZpcZl0qg", "FApX-M_DrDc", "uPHQb5JBppY", "FLpiDFE991g", "gRWZRz-Gf00", "dyVcCCa1hv4", "jAMGMKlBSV0", "hpukfqMhI1c", undefined, "wnWkseF6rvM"]
   searchFunction = (text) => {
-    fetch(url + apiKey + word + text).then((resp) => {
+    fetch(url+apiKey+word+text).then((resp) => { 
       resp.json().then((jsonResp) => {
-        let aux = []
-        jsonResp.items.forEach(element => {
-          aux.push(element.id.videoId)
-        });
-        this.response = aux
+         let aux = []
+         let titleAux = []
+         jsonResp.items.forEach(element => {
+           aux.push(element.id.videoId )
+           let titleToPush = element.snippet.title.match(/.{1,35}/g)
+           titleAux.push( titleToPush[0] + (titleToPush[1] === undefined ? "" : "..."))
+         });                  
+         this.response = aux
+         this.videdoTitles = titleAux
+         console.log(titleAux)
+         console.log(aux)
+         this.mostrarVideos = true
+         this.videoSeleccionado = false
       })
     })
+  
+
   }
+
+  
+  openVideo(video) {
+    this.mostrarVideos = false
+    this.videoSeleccionado = true
+    this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl("https://www.youtube.com/embed/" + video.split("/")[4]);    
+  }
+
 }
