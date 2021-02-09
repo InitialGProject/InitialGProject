7948a5a8579c6a826ab52909ae11196c2b2d7b97
Juan Sanz <juansanzvalencia00@gmail.com>
2021-02-09 10:20:41 +0100

    vista-comentario_0.1

--- src/app/foro/detalle-foro/detalle-foro.component.ts
+++ src/app/foro/detalle-foro/detalle-foro.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+// Modelo de Comentarios
+import { Comentarios } from '../models/comentarios';
+
+// Servicio de Entradas y sus Comentarios
+import { ForoService } from '../services/foro.service';
+
 @Component({
   selector: 'app-detalle-foro',
   templateUrl: './detalle-foro.component.html',
   styleUrls: ['./detalle-foro.component.scss']
 })
+
 export class DetalleForoComponent implements OnInit {
 
-  constructor() { }
+  comentarios: Comentarios;
+
+  constructor(private servicioForo: ForoService) { }
 
   ngOnInit(): void {
+    this.dameComentarios();
   }
 
+  dameComentarios(): void {
+    this.servicioForo.getComentarios()
+      .subscribe(
+        infoComentario => {
+          this.comentarios = infoComentario;
+          console.log(infoComentario);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
