1ed8f84c77072cd3a1079c3c04669ea20e52c945
mithjack <alexlopez168@hotmail.com>
2021-02-09 08:28:16 +0100

    foro service

--- src/app/foro/services/foro.service.ts
+++ src/app/foro/services/foro.service.ts
@@ -5,8 +5,8 @@ import { Observable } from 'rxjs';
 import { Entradas } from '../models/entradas';
 import { Comentarios } from '../models/comentarios';
 
-const entradas = 'http://localhost:8080/entradas';
-const comentarios = 'http://localhost:8080/comentarios';
+const entradas = 'http://alum3.iesfsl.org/api/entradas';
+const comentarios = 'http://alum3.iesfsl.org/api/comentarios';
 
 @Injectable({
   providedIn: 'root'
