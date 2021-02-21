a7eafaa787d4067651e90b696a367a34d846b80e
danedelea <danedelea@github .com>
2021-02-21 17:19:54 +0100

    inicio 90%

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -3,10 +3,14 @@ import { Component, OnInit } from '@angular/core';
 // Modelos
 import { User } from './../_models/user';
 import { Noticia } from '../noticias/models/noticia';
+import { Juegos } from '../juegos/models/juegos';
 
 // Servicios
 import { NoticiasService } from '../noticias/services/noticias.service';
 import { AccountService } from './../_services/account.service';
+import { JuegosService } from '../juegos/services/juegos.service';
+
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -17,16 +21,22 @@ import { AccountService } from './../_services/account.service';
 export class InicioComponent implements OnInit {
 
   noticias: Noticia;
+  tipo: {clase: string};
+  juego: Juegos;
   user: User;
 
   constructor(
     private accountService: AccountService,
     private servicioNoticias: NoticiasService,
+    private servicioJuegos: JuegosService, 
+    private rutaActiva: ActivatedRoute
 
   ) { this.user = this.accountService.userValue; }
 
   ngOnInit(): void {
     this.dameNoticias();
+    this.dameJuegos();
+
   }
 
   dameNoticias(): void {
@@ -35,6 +45,23 @@ export class InicioComponent implements OnInit {
       .subscribe(
         data => {
           this.noticias = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  dameJuegos(): void {
+
+    this.tipo = {
+      clase: this.rutaActiva.snapshot.params.clase
+    };
+    
+    this.servicioJuegos.getAll()
+      .subscribe(
+        data => {
+          this.juego = data;
           console.log(data);
         },
         error => {
