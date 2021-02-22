253da42f8e55168a0535429431bb1afe805372bb
mithjack <alexlopez168@hotmail.com>
2021-02-22 10:57:22 +0100

    fix noticias (server) y ruta (rela)

--- src/app/noticias/services/noticias.service.ts
+++ src/app/noticias/services/noticias.service.ts
@@ -4,8 +4,8 @@ import { Observable } from 'rxjs';
 
 import { Noticia } from '../models/noticia';
 
-// const baseUrl = 'http://alum3.iesfsl.org/api/noticias';
-const baseUrl = 'http://localhost:8080/noticias';
+const baseUrl = 'http://alum3.iesfsl.org/api/noticias';
+// const baseUrl = 'http://localhost:8080/noticias';
 
 @Injectable({
   providedIn: 'root'
