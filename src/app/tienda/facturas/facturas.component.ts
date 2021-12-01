6d7373dfcbf2d0e04ece0366c722cc98f291f013
mithjack <alexlopez168@hotmail.com>
2021-12-01 13:24:22 +0100

    corrección compras

--- src/app/tienda/facturas/facturas.component.ts
+++ src/app/tienda/facturas/facturas.component.ts
@@ -82,10 +82,11 @@ export class FacturasComponent implements OnInit {
 
       // Add image Canvas to PDF
       const bufferX = 15;
-      const bufferY = 15;
+      const bufferY = 95;
       const imgProps = (doc as any).getImageProperties(img);
       const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
       const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.text(["INITIAL G","NIF:123456789ASDF","C/Falsa de prueba 666", "VALENCIA", "FACTURA Nº"+id], 10, 15);
       doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
       return doc;
     }).then((docResult) => {
